eee355afacc85def9f591480c1d1b138e03f31b8
FabioVCoelho <angrazito@gmail.com>
2017-08-19 10:28:09 -0300

    brModeloNext

---
+++ src/ufsc/sisinf/brmodelo2all/util/AppConstants.java
@@ -0,0 +1,11 @@
+package ufsc.sisinf.brmodelo2all.util;
+
+public class AppConstants {
+
+	public static final int TEXT_FIELD = 0;
+
+	public static final int CHECK_BOX = 1;
+
+	public static final int COMBO_BOX = 2;
+
+}
